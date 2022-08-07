10222aab58a4ee25618bb56be37570461761e123
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2022-08-07 13:44:54 -0400

    Atualização do dia 07/08/2022 - Lógica do Programa ok

--- app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
+++ app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.calculoaposentadoria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +14,28 @@ class MainActivity : AppCompatActivity() {
         val txtIdade: EditText  = findViewById(R.id.txt_idade)
         val btnCalcular: Button = findViewById(R.id.btn_calcular)
         val txtResultado: TextView = findViewById(R.id.txt_resultado)
+
+
+        spnSexo.adapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, listOf("masculino", "feminino"))
+
+        btnCalcular.setOnClickListener{
+            // //aqui vai o código que será executado quando houver um click do botão
+
+            //Capturando o sexo selecionado
+            val sexo =spnSexo.selectedItem as String
+            // Capturando a idade digitada
+            val idade = txtIdade.text.toString().toInt()
+
+            var resultado = 0
+
+            if (sexo == "masculino"){
+                resultado = 65 - idade
+            }else{
+                resultado = 60 - idade
+            }
+
+            txtResultado.text = "Faltam $resultado anos para você se aposentar."
+        }
 
     }
 }
