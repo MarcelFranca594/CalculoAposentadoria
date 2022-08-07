8828e11ee320010ae1d531ce3af03a85bccbab54
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2022-08-07 13:21:42 -0400

    Atualização do dia 07/08/2022

--- app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
+++ app/src/main/java/com/example/calculoaposentadoria/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.calculoaposentadoria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val spnSexo: Spinner = findViewById(R.id.spn_sexo)
+        val txtIdade: EditText  = findViewById(R.id.txt_idade)
+        val btnCalcular: Button = findViewById(R.id.btn_calcular)
+        val txtResultado: TextView = findViewById(R.id.txt_resultado)
+
     }
 }
